3ec93b52ad9d8497be8450a5cc7376eee00dbc30
SPDoctor <BillA@flosim.com>
2016-01-30 11:41:34 +0000

    Initial commit

---
+++ Office365Cordova/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,23 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.inappbrowser": "0.6.0",
+    "cordova-plugin-whitelist": "1.1.0"
+}
+// BOTTOM OF METADATA
+});
